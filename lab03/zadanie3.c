c075ab0a09b3558e94f087b3a5f28b23262d1fa5
Łucja Leśna <lesna.lucja@gmail.com>
2023-10-29 14:49:24 +0100

    zadanie3

---
+++ lab03/zadanie3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int liczba1, liczba2;
+
+    printf("podaj pierwszą liczbę: ");
+    scanf ("%d", &liczba1);
+    printf("podaj drugą liczbę: ");
+    scanf ("%d", &liczba2);
+
+    if (liczba1 > liczba2) {
+        printf("większa jest pierwsza liczba: %d", liczba1);
+    }
+    else {
+        printf("większa jest druga liczba: %d", liczba2);
+    }
+
+return 0;
+}
