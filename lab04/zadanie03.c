fc9a4fffeee329b92767e04a6d34f097eb2a5163
Łucja Leśna <lesna.lucja@gmail.com>
2023-11-07 22:53:00 +0100

    dodanie zadań

---
+++ lab04/zadanie03.c
@@ -0,0 +1,20 @@
+/*Napisz program który wczyta od użytkownika parę liczb a,b i zwróci pierwszą liczbę c, taką że a do potęgi c jest większe od b.*/
+
+#include <stdio.h>
+
+int main()
+{
+    unsigned int a, b, c = 0, wynik = 1;
+    
+    printf("Podaj dwie liczby naturalne:\n");
+    scanf("%u%u", &a, &b);
+
+    while (wynik < b) {
+        wynik *= a;
+        c++;
+    }
+
+    printf("%u do potęgi %u jest większe od %u\n", a, c, b);
+
+    return 0;
+}
