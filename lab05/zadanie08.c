c5e4f7bafbfda7741a10ab106df17345845ee8cf
Łucja Leśna <lesna.lucja@gmail.com>
2023-11-25 19:07:01 +0100

    zadanie 8

---
+++ lab05/zadanie08.c
@@ -0,0 +1,57 @@
+/*Napisz funkcję, która przyjmuje jako argumenty liczby d,m,r i sprawdzi, czy tworzą poprawną datę (d-dzień, m-miesiąc, r-rok).*/
+
+#include <stdio.h>
+
+int czy_poprawna_data(int dzien, int miesiac, int rok)
+{
+    if (miesiac >=1 && miesiac <= 12) {
+        if (miesiac == 1 || miesiac == 3 || miesiac == 5 || miesiac == 7 || miesiac == 8 || miesiac == 10 || miesiac == 12) {
+            if (dzien >= 1 && dzien <= 31) {
+                printf("Data %d.%d.%d jest poprawna.\n", dzien, miesiac, rok);
+            }   
+            else {
+                printf("Podano niepoprawny dzień.\n");
+            }
+        }
+        else if (miesiac == 2) {
+            if (((rok % 4 == 0 && rok % 100 != 0) || (rok % 400 == 0)) && (dzien >= 1 && dzien <= 29)) {
+                printf("Data %d.%d.%d jest poprawna.\n", dzien, miesiac, rok);
+            }
+            else if ((dzien >= 1 && dzien <= 28)) {
+                printf("Data %d.%d.%d jest poprawna.\n", dzien, miesiac, rok);
+            } 
+            else {
+                printf("Podano niepoprawny dzień.\n");
+            }
+        }
+        else {
+            if (dzien >= 1 && dzien <= 30) {
+                printf("Data %d.%d.%d jest poprawna.\n", dzien, miesiac, rok);
+            }   
+            else {
+                printf("Podano niepoprawny dzień.\n");
+            }
+        }
+    }
+    else {
+        printf("Podano niepoprawny miesiąc.\n");
+    }
+}
+
+int main() 
+{
+    int dzien, miesiac, rok;
+
+    printf("Podaj dzień: ");
+    scanf("%d", &dzien);
+    
+    printf("Podaj miesiąc: ");
+    scanf("%d", &miesiac);
+    
+    printf("Podaj rok: ");
+    scanf("%d", &rok);
+
+    czy_poprawna_data(dzien, miesiac, rok);
+
+    return 0;
+}
