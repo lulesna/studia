3b3ffbf012f34f5049433e2d34e01aeb40e7399a
Łucja Leśna <lesna.lucja@gmail.com>
2023-11-25 18:59:45 +0100

    zadanie 5

--- lab05/zadanie05.c
+++ lab05/zadanie05.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-int funkcja(unsigned int a1, unsigned int b1, unsigned int a2, unsigned int b2, unsigned int a3, unsigned int b3)
+int funkcja(unsigned int a1, unsigned int b1, unsigned int a2, unsigned int b2, unsigned int a3)
 {
     if (b1 > a1 && b2 > a2) {
         if (b1 < a2 || b2 < a1) {
@@ -11,7 +11,7 @@ int funkcja(unsigned int a1, unsigned int b1, unsigned int a2, unsigned int b2, 
         }
         else if (a1 > a2) {
             a3 = a1;
-            printf("%d\n", a3);
+            printf("a3 = %d\n", a3);
         }
         else {
             a3 = a2;
@@ -25,12 +25,12 @@ int funkcja(unsigned int a1, unsigned int b1, unsigned int a2, unsigned int b2, 
 
 int main() 
 {
-    unsigned int a1, b1, a2, b2, a3, b3;
+    unsigned int a1, b1, a2, b2, a3;
 
     printf("Podaj cztery liczby naturalne oddzielone spacjami (a1 b1 a2 b2): ");
     scanf("%u %u %u %u", &a1, &b1, &a2, &b2);
 
-    funkcja(a1, b1, a2, b2, a3, b3);
+    funkcja(a1, b1, a2, b2, a3);
 
     return 0;
 }
