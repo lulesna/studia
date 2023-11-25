7a26c7a13e49caf4eea547c9b5b1f90505e279d4
Łucja Leśna <lesna.lucja@gmail.com>
2023-11-25 17:55:37 +0100

    zadanie 5

--- lab05/zadanie05.c
+++ lab05/zadanie05.c
@@ -7,20 +7,20 @@ int funkcja(unsigned int a1, unsigned int b1, unsigned int a2, unsigned int b2, 
     if (b1 > a1 && b2 > a2) {
         if (b1 < a2 || b2 < a1) {
             printf("-1\n");
-            return -1;
+            return 1;
+        }
+        else if (a1 > a2) {
+            a3 = a1;
+            printf("%d\n", a3);
         }
         else {
-            if (a1 < a2 || a1 == a2) {
-                a3 = a2;
-            }
-            else {
-                a3 = a1;
-            }
+            a3 = a2;
+            printf("a3 = %d\n", a3);
         }
-        printf("%d\n", a3);
+        
     }
     else {
-
+        printf("Błędne dane, b1 powinno być większe od a1, i b2 powinno być większe od a2.\n");
     }
 }
 
