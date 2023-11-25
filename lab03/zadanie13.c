e9eb867a302e5024658c9f2049ffd25249b50108
Łucja Leśna <lesna.lucja@gmail.com>
2023-11-25 17:54:56 +0100

    poprawki

--- lab03/zadanie13.c
+++ lab03/zadanie13.c
@@ -6,8 +6,8 @@ int main()
 {
     int odp;
 
-    printf ("Dla 0 radianów wybierz 1.\nDla 1/6 Pi rad wybierz 2.\nDla 1/4 Pi rad wybierz 3.\nDla 1/3 Pi rad wybierz 4.\nDla 1/2 Pi rad wybierz 5.\n");
-    scanf ("%d", &odp);
+    printf("Dla 0 radianów wybierz 1.\nDla 1/6 Pi rad wybierz 2.\nDla 1/4 Pi rad wybierz 3.\nDla 1/3 Pi rad wybierz 4.\nDla 1/2 Pi rad wybierz 5.\n");
+    scanf("%d", &odp);
 
     if (odp == 1) {
         printf("0 radianów to 0 stopni.\n");
