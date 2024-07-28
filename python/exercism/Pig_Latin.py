b9b1a317bb0319063d18462795d1e2bd60afa055
Łucja <149304509+llesna@users.noreply.github.com>
2024-07-28 16:33:18 +0200

    Delete python directory

--- python/exercism/Pig_Latin.py
+++
@@ -1,8 +0,0 @@
-def translate(text):
-    if text.startswith('a' or 'e' or 'i' or 'o' or 'u' or 'xr' or 'yr'):
-        return text + "ay"
-    if text.startswith('ch' or 'st'):
-        return text + "ay"
-    if text.startswith('*qu'):
-        return
-    if text[1] = "y" or :
--- python/exercism/Square_Root.py
+++
@@ -1,5 +0,0 @@
-def square_root(number):
-    if number >= 0:
-        return round(number ** 0.5)
-    else:
-        raise ValueError("Invalid number")
