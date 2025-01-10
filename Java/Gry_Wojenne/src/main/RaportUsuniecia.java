4cbbaa4914fe604bdcfc682f75782a6315c92cbe
Łucja <149304509+llesna@users.noreply.github.com>
2025-01-10 18:15:15 +0100

    dodanie funkcji

--- Java/Gry_Wojenne/src/main/RaportUsuniecia.java
+++ Java/Gry_Wojenne/src/main/RaportUsuniecia.java
@@ -1,27 +1,35 @@
 public class RaportUsuniecia extends Raport {
     private final General general;
-    private final int liczbaZolnierzyPrzed;
+    private int liczbaZolnierzyPrzed;
+    private int liczbaZolnierzyPo;
 
     public RaportUsuniecia(General general) {
         super("USUNIECIE");
         this.general = general;
+    }
+
+    public void zapiszStanPrzed() {
         this.liczbaZolnierzyPrzed = general.getArmia().size();
+    }
+
+    public void zapiszStanPo() {
+        this.liczbaZolnierzyPo = general.getArmia().size();
     }
 
     @Override
     public String generuj() {
-        int usunieciZolnierze = liczbaZolnierzyPrzed - general.getArmia().size();
+        int usunieciZolnierze = liczbaZolnierzyPrzed - liczbaZolnierzyPo;
 
         return getNaglowek() +
                 String.format("""
                 Generał: %s
-                Liczba żołnierzy przed usuniciem: %d
+                Liczba żołnierzy przed usunięciem: %d
                 Liczba żołnierzy po usunięciu: %d
                 Usunięto żołnierzy: %d
                 """,
                         general.getNazwa(),
                         liczbaZolnierzyPrzed,
-                        general.getArmia().size(),
+                        liczbaZolnierzyPo,
                         usunieciZolnierze
                 );
     }
